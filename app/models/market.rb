1e1e00788fe1eb7f2be7d0c3e17d66e971f86748
Victor Pelle <victor.pelle97@gmail.com>
2022-12-01 17:33:01 +0100

    debug

--- app/models/market.rb
+++ app/models/market.rb
@@ -1,8 +1,10 @@
 class Market < ApplicationRecord
   has_one_attached :photo
-  has_many :tickets
   has_many :market_products, dependent: :destroy
 
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
+
+  def horaires
+  end
 end
