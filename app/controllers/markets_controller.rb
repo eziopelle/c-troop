ea1301b696d52bdf7e79c306a5c11ccfac71e05b
Victor Pelle <victor.pelle97@gmail.com>
2022-12-02 16:43:26 +0100

    debug

--- app/controllers/markets_controller.rb
+++ app/controllers/markets_controller.rb
@@ -4,14 +4,14 @@ class MarketsController < ApplicationController
 
     # Marker pour géocoder (vic)
     @markers = @markets.geocoded.map do |market|
-      image = market.total_price > MarketProduct.average_price ? "ping-vert.png" : "ping-rouge.png"
-      color = market.total_price > MarketProduct.average_price ? "green" : "red"
+      image = market.total_price < MarketProduct.average_price ? "ping-vert.png" : "ping-rouge.png"
+      color = market.total_price < MarketProduct.average_price ? "green" : "red"
       {
         lat: market.latitude,
         lng: market.longitude,
         info_window: render_to_string(partial: "info_window", locals: { market: market }),
         image_url: helpers.asset_url(image),
-        total_price: market.total_price,
+        total_price: market.total_price.round(2),
         color: color
       }
     end
