4578d707f9a46313ef7caf8d4dcd204d2c1008b9
Mario Sanchez Jaramillo <mario.sanchez.jar@gmail.com>
2022-12-05 11:49:10 +0100

    text_detection_done

--- Gemfile
+++ Gemfile
@@ -11,8 +11,8 @@ gem "geocoder"
 #GemMaxence
 gem "cloudinary"
 
-# tesseract
-gem 'rtesseract'
+# google vision v1
+gem 'google-cloud-vision-v1'
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.0.4"
--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,4 +1,7 @@
 class TicketsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [ :text_detection ]
+  require "google/cloud/vision/v1"
+  
   def index
     @tickets = policy_scope(Ticket)
     text_detection
@@ -29,13 +32,33 @@ class TicketsController < ApplicationController
   end
 
   def text_detection
-    path = "#{Rails.root}/app/assets/images/test_ticket.jpeg"
-    # path = @tickets.last.photo.url
-    @image = RTesseract.new(path, lang: 'fra')
+    client = Google::Cloud::Vision::V1::ImageAnnotator::Client.new
+
+    # file_path = Rails.root.join('app','assets','images', 'invoice.jpeg').to_s
+    # file_path = 'https://res.cloudinary.com/dbjv42wpc/image/upload/v1670000239/development/kwdb4med5pqwa436gnnxndh0etbm.jpg'
+    file_path = @tickets.last.photo.url
+    @response = client.document_text_detection image: file_path
     @words = []
-    @image.to_box.each do |word|
-      @words << word[:word]
+
+    @response.responses.each do |res|
+      res.text_annotations.each do |annotation|
+        @words << annotation.description
+      end
     end
+
+    biggest_number
+  end
+
+  def biggest_number
+    regex = /([^\s]*)(€|EUR)/
+    @amounts = []
+    @words.each do |word|
+      @amounts << word.scan(regex).join.split('€') if word.match(regex)
+    end
+    @amounts[0].map! do |amount|
+      amount.to_f
+    end
+    @biggest_number = @amounts[0].max
   end
 
   private
