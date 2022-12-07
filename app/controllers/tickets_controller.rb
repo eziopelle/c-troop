2051e33a9bd2b01f21a74d1ce3feb5fb1b7ff326
Mario Sanchez Jaramillo <mario.sanchez.jar@gmail.com>
2022-12-07 10:10:16 +0100

    start

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -25,6 +25,10 @@ class TicketsController < ApplicationController
     end
   end
 
+  def update
+
+  end
+
   def destroy
     authorize @ticket
     @ticket = ticket.find(params[:market_id])
@@ -34,8 +38,6 @@ class TicketsController < ApplicationController
   def text_detection
     client = Google::Cloud::Vision::V1::ImageAnnotator::Client.new
 
-    # file_path = Rails.root.join('app','assets','images', 'invoice.jpeg').to_s
-    # file_path = 'https://res.cloudinary.com/dbjv42wpc/image/upload/v1670000239/development/kwdb4med5pqwa436gnnxndh0etbm.jpg'
     file_path = @tickets.last.photo.url
     @response = client.document_text_detection image: file_path
     @words = []
@@ -59,6 +61,12 @@ class TicketsController < ApplicationController
       amount.to_f
     end
     @total_price = @amounts[0].max
+  end
+
+  def detect_brand_address
+    match_data = @words.match(/(?:(?:\+|00)33|0)\s*[1-9](?:[\s.-]*\d{2}){4}/)
+    match_data[0].gsub(" ","")
+    Market.find_by(phone_number: match_data[0])
   end
 
   private
