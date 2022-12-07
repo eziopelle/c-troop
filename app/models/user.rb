a1f0ae3b02216eb1a4cb24b39dd507a69712dbfd
Maxence Comptdaer <comptdaer.maxence23@gmail.com>
2022-12-07 09:17:42 +0100

    refonte dashboard

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_one_attached :photo
   has_many :tickets, dependent: :destroy
-
+  has_many :markets, through: :tickets
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
