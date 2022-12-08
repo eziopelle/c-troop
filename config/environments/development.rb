abe6ba212afe016e186df76845a36afe6ebea284
Victor Pelle <victor.pelle97@gmail.com>
2022-12-08 17:37:01 +0100

    master push

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,6 +62,10 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  #vic
+  config.hosts.clear
+
+
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
